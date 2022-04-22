621ccc50168c9f2c472c8665ec168c5cb0553de5
alfonsomendezc <alfonsomendez2209@gmail.com>
2022-04-22 23:01:06 +0000

    v2

--- src/js/index.js
+++ src/js/index.js
@@ -27,12 +27,12 @@ const SecondsCounter = (props) => {
 let counter = 0;
 
 setInterval(function () {
-	const one = Math.floor(counter / 1);
-	const two = Math.floor(counter / 10);
-	const three = Math.floor(counter / 100);
-	const four = Math.floor(counter / 1000);
-	const five = Math.floor(counter / 10000);
-	const six = Math.floor(counter / 100000);
+	const one = counter % 10;
+	const two = Math.floor(counter / 10) % 10;
+	const three = Math.floor(counter / 100) % 10;
+	const four = Math.floor(counter / 1000) % 10;
+	const five = Math.floor(counter / 10000) % 10;
+	const six = Math.floor(counter / 100000) % 10;
 	counter++;
 	ReactDOM.render(
 		<SecondsCounter
